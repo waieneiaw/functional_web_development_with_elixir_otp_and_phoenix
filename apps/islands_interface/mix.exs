084b2ae6bdb8ec20053cd1e7b4e38e27a65552a3
waieneiaw <15177530+waieneiaw@users.noreply.github.com>
2022-08-18 21:51:19 +0000

    feat: add `engine` to deps in `interface`

--- apps/islands_interface/mix.exs
+++ apps/islands_interface/mix.exs
@@ -49,7 +49,8 @@ defmodule IslandsInterface.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:islands_engine, in_umbrella: true}
     ]
   end
 
